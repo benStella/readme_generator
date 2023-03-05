686f31b866025ab9f44cb8bad8345c735529a744
ben <benstella13@gmail.com>
2023-03-05 16:40:37 -0500

    properly creates readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Readme Generator
+## Description
+Readme generator is a command line app for creating basic readme's
+## Installation
+open in your code editor and press compile
+## Usage
+Create basic readme files## Credits
+Benjamin Stella
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 string generateReadme();
@@ -7,35 +8,60 @@ string generateReadme();
 int main()
 {
     generateReadme();
+    return 0;
 }
 
 string generateReadme()
 {
+    // variables
     string readmeTitle;
     string readmeDescription;
     string readmeTableOfContents;
     string installAndRun;
+    string useProject;
+    string credits;
+    string license;
 
+    // input readme title
+    cout << "title of README: ";
+    getline(cin, readmeTitle);
 
-    cout << "title of README:";
-    cin >> readmeTitle;
+    // input readme description
+    cout << "README Description: ";
+    getline(cin, readmeDescription);
 
-    cout << "README Description:";
-    cin >> readmeDescription;
-    
-    cout << "readmeTableOfContents";
-    cin >> readmeTableOfContents;
+    // input how to run and install program
+    cout << "how to install: ";
+    getline(cin, installAndRun);
 
-    cout << "how to install and run;";
-    cin >> installAndRun;
-    
-    
-    
+    // usage
+    cout << "usage: ";
+    getline(cin, useProject);
+
+    // input the credits
+    cout << "credits: ";
+    getline(cin, credits);
+
+    // generate and open readme
     fstream readme;
     readme.open("README.md", ios::out);
 
-    if(readme.is_open()) {
-        readme << "# " << readmeTitle;
+    // if readme is open write all inputted data to it
+    if (readme.is_open())
+    {
+        readme << "# " << readmeTitle << endl
+               << "## "
+               << "Description" << endl
+               << readmeDescription << endl
+               << "## "
+               << "Installation" << endl
+               << installAndRun << endl
+               << "## "
+               << "Usage" << endl
+               << useProject << endl
+               << "## "
+               << "Credits" << endl
+               << credits;
         readme.close();
     }
 }
