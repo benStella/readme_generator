f44a6385693fb7c5d19a8e0b3e70c72286cbc2c8
ben <benstella13@gmail.com>
2023-03-06 18:29:34 -0500

    modularized code

---
+++ generateReadme.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void generateReadme()
+{
+    // variables
+    string readmeTitle;
+    string readmeDescription;
+    string readmeTableOfContents;
+    string installAndRun;
+    string useProject;
+    string credits;
+    string license;
+
+    // input readme title
+    cout << "title of README: ";
+    getline(cin, readmeTitle);
+
+    // input readme description
+    cout << "README Description: ";
+    getline(cin, readmeDescription);
+
+    // input how to run and install program
+    cout << "how to install: ";
+    getline(cin, installAndRun);
+
+    // usage
+    cout << "usage: ";
+    getline(cin, useProject);
+
+    // input the credits
+    cout << "credits: ";
+    getline(cin, credits);
+
+    // generate and open readme
+    fstream readme;
+    readme.open("README.md", ios::out);
+
+    // if readme is open write all inputted data to it
+    if (readme.is_open())
+    {
+        readme << "# " << readmeTitle << endl
+               << "## "
+               << "Description" << endl
+               << readmeDescription << endl
+               << "## "
+               << "Installation" << endl
+               << installAndRun << endl
+               << "## "
+               << "Usage" << endl
+               << useProject << endl
+               << "## "
+               << "Credits" << endl
+               << credits;
+        readme.close();
+    }
+    cout << "readme created" << endl;
+}
